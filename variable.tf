6cb2264121622e1c76d4f88d869076aa4963a3d1
srujanKs5 <127977584+srujanKs5@users.noreply.github.com>
2023-04-20 17:06:47 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "n_virginia_key"
+  default     = "jenkins"
 }
 
 variable "instance_type" {
